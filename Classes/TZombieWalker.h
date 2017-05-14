05eaa348e8fb67bd6544be2c4179de4f5be04e89
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-05-14 15:16:17 +0300

    Create TZombieWalker.h

---
+++ Classes/TZombieWalker.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "TZombie.h"
+//Медленнее ZombieMusic, т.к. не имеет таких крутых наушников и вообще уже засыхает
+class TZombieWalker :
+	public TZombie 
+{	
+public:
+	TZombieWalker(size_t XPos, size_t YPos);
+	void Live();
+}; 
