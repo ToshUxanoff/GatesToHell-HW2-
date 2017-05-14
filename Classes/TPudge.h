000c309f0c3765f9756c4092bb04b5b4bad36ba9
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-05-14 15:02:42 +0300

    Create TPudge.h

---
+++ Classes/TPudge.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <TCreature.h>
+USING_NS_CC;
+class TPudge :
+	public TCreature
+{
+public:
+	TPudge(size_t XPos, size_t YPos, const std::string& NameSprite);
+	void ChopChop();
+};
