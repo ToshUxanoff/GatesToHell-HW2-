b2ac916e373ef7f15f97bfe1d109251b036f8412
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-05-15 20:19:47 +0300

    Update TCreature.h

--- Classes/TCreature.h
+++ Classes/TCreature.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "cocos2d.h"
+#include <vld.h>
 USING_NS_CC;
-//Базовый класс, от которого наследуются остальные
+
 class TCreature
 {
 	cocos2d::Sprite* Model;
@@ -11,15 +12,18 @@ class TCreature
 	float Height = Model->getContentSize().height;
 public:
 	TCreature();
-	TCreature(size_t X, size_t Y, cocos2d::Sprite* Spr); 
+	TCreature(size_t X, size_t Y, cocos2d::Sprite* Spr);
 	~TCreature() = default;
-
 	float GetModelWidth();
 	float GetModelHeight();
-	
-	size_t GetX();	
+
+	size_t GetX();
 	cocos2d::Sprite * GetModel();
 
 	void GainX(size_t x);
 	void GainY(size_t y);
+	virtual void Live();
+	virtual void ChopChop();
+	virtual void CreakCreak();
+
 };
